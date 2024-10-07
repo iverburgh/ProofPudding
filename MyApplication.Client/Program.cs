3c590e5f9e7890f90e2bfdcdf9f27fdd71e2a146
Ivo Verburgh <I.Verburgh@4dotnet.nl>
2024-10-07 22:11:27 +0200

    Added deploying and tracing application insights

--- MyApplication.Client/Program.cs
+++ MyApplication.Client/Program.cs
@@ -1,9 +1,19 @@
+using BlazorApplicationInsights;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+
+
+builder.Services.AddBlazorApplicationInsights(x =>
+{
+    x.ConnectionString = "InstrumentationKey=bfd00310-cf98-4fb9-bc39-4d0d1c6e2320;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/;ApplicationId=d875b245-1764-4e60-b587-a6038abd3435";
+});
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+
 
 await builder.Build().RunAsync();
--- MyApplication/Program.cs
+++ MyApplication/Program.cs
@@ -1,8 +1,14 @@
+using BlazorApplicationInsights;
 using MudBlazor.Services;
-using MyApplication.Client.Pages;
 using MyApplication.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddBlazorApplicationInsights(x =>
+{
+    x.ConnectionString = "InstrumentationKey=bfd00310-cf98-4fb9-bc39-4d0d1c6e2320;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/;ApplicationId=d875b245-1764-4e60-b587-a6038abd3435";
+});
+
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
